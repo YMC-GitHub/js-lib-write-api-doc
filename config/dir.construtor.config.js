5603d2a12bf1a0adc94b891b2994c698d778aa24
yemiancheng <ymc.github@gmail.com>
2020-01-26 16:03:36 +0800

    build: update config

--- config/dir.construtor.config.js
+++ config/dir.construtor.config.js
@@ -1,14 +1,13 @@
-
-const path = require('path')
-const dirMapConfig = require('./dir.map.config')
-const rootPath = path.resolve(__dirname, '..')
-const resolve = file => path.resolve(rootPath, file)
+const path = require('path');
+const dirMapConfig = require('./dir.map.config');
+const rootPath = path.resolve(__dirname, '..');
+const resolve = file => path.resolve(rootPath, file);
 
 module.exports = {
-  //save the source code (for dev)
+  // save the source code (for dev)
   src: resolve(dirMapConfig.src),
-  //save the test source code (for test)
+  // save the test source code (for test)
   test: resolve(dirMapConfig.test),
-  //save the build ouput code by rollup
-  dist: resolve(dirMapConfig.dist),
-}
+  // save the build ouput code by rollup
+  dist: resolve(dirMapConfig.dist)
+};
--- config/dir.map.config.js
+++ config/dir.map.config.js
@@ -4,5 +4,5 @@ module.exports = {
   // the dir name to save the test source code (for test)
   test: 'test',
   // the dir name to save the build ouput code by rollup
-  dist: 'lib'
+  dist: 'dist'
 };

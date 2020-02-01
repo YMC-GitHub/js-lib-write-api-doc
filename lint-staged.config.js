346bd97c3bbfb7023ae8c7bbe13e6dae02c928d4
yemiancheng <ymc.github@gmail.com>
2020-02-01 09:44:38 +0800

    build: update lint-staged config

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,3 +1,10 @@
 module.exports = {
-  'src/*.{js,json,css}': ['prettier --write', 'eslint --fix', 'git add .']
+  'src/*.{js,json,css}': [
+    // tells lint-staged to run prettier lib
+    // "prettier --write",
+    // tells lint-staged to run eslint lib with --fix arg
+    'eslint --fix'
+    // tells lint-staged  to git add all
+    // 'git add .'
+  ]
 };
